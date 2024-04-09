32eda737a74f6e8e03f19b9439d1952e4cd6180b
el3shka <harv3st3r666@gmail.com>
2024-04-10 00:06:57 +0600

    2nd ver fish

--- src/model/StatusTask.java
+++
@@ -1,8 +0,0 @@
-
-package model;
-
-public enum StatusTask {
-    NEW,
-    IN_PROGRESS,
-    DONE
-}
--- src/model/TaskType.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public enum TaskType {
-    EPIC,
-    TASK,
-    SUBTASK;
-}
-
