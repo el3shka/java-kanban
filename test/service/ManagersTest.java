4033c6e4c3dff7851bac81ba8ed0083a7878d6e7
Vitaliy P <94363715+el3shka@users.noreply.github.com>
2024-05-29 02:27:39 +0600

    Update ManagersTest.java

--- test/service/ManagersTest.java
+++ test/service/ManagersTest.java
@@ -9,17 +9,17 @@ import java.util.List;
 
 
 class ManagersTest {
-    @DisplayName("Возврат инициализации и работы InMemoryHistoryManager")
+    @DisplayName("Return init and working InMemoryHistoryManager")
     @Test
     void shouldBeReturnInitAndWorkInMemoryHistoryManager() {
         InMemoryHistoryManager historyManager = Managers.getHistoryManager();
         historyManager.add(new Task("A", "b"));
         List<Task> history = historyManager.getHistory();
         Assertions.assertEquals(1, history.size(),
-                "Менеджер вернул нерабочий InMemoryHistoryManager");
+                "Manager return not working InMemoryHistoryManager");
     }
 
-    @DisplayName("Возврат инициализации и работы InMemoryTaskManager")
+    @DisplayName("Return init and working InMemoryTaskManager")
     @Test
     void shouldBeReturnInitAndWorkInMemoryTaskManager() {
         InMemoryTaskManager testTaskManager = Managers.getDefault();
@@ -27,11 +27,10 @@ class ManagersTest {
         InMemoryTaskManager taskManager = new InMemoryTaskManager(historyManager);
 
         Assertions.assertEquals(testTaskManager.allTask, taskManager.allTask,
-                "Managers не вернул InMemoryTaskManager из getDefault");
+                "Managers not return InMemoryTaskManager from getDefault");
         Assertions.assertEquals(testTaskManager.allSubtask, taskManager.allSubtask,
-                "Managers не вернул InMemoryTaskManager из getDefault");
+                "Managers not return InMemoryTaskManager from getDefault");
         Assertions.assertEquals(testTaskManager.allEpics, taskManager.allEpics,
-                "Managers не вернул InMemoryTaskManager из getDefault");
-
+                "Managers not return InMemoryTaskManager from getDefault");
     }
 }
