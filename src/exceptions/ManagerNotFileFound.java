9c1e9d0e45bc774ca63f2de36d41888ac467f3f2
Vitaliy P <94363715+el3shka@users.noreply.github.com>
2024-05-30 00:00:09 +0600

    Update ManagerNotFileFound.java

--- src/exceptions/ManagerNotFileFound.java
+++ src/exceptions/ManagerNotFileFound.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+//review - Наверное имелось в виду ManagerFileNotFoundException
+//my comms - Необходимо изменить название класса? // В ТЗ ничего про это не сказано.
+
 public class ManagerNotFileFound extends RuntimeException {
     public ManagerNotFileFound(String message) {
         super(message);
